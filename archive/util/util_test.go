18ec7d875c688c571cf2174ad76251c784a58027
Jordan Brockopp <jdbro94@gmail.com>
2017-02-08 15:15:27 -0600

    Update fromfilename (#6)
    * added methods to handle suffix of .tgz and .tar.gz
    
    * added tests for updated fromfilename method
    
    * converted .tgz blocks into one if block
    
    * moved second half of or to inline

--- archive/util/util_test.go
+++ archive/util/util_test.go
@@ -14,6 +14,16 @@ func TestArchive(t *testing.T) {
 			g.Assert(err == nil).IsTrue("failed to determine .tar suffix")
 		})
 
+		g.It("Should return tgzArchive for .tgz", func() {
+			_, err := FromFilename("filename.tgz")
+			g.Assert(err == nil).IsTrue("failed to determine .tgz suffix")
+		})
+
+		g.It("Should return tgzArchive for .tar.gz", func() {
+			_, err := FromFilename("filename.tar.gz")
+			g.Assert(err == nil).IsTrue("failed to determine .tar.gz suffix")
+		})
+
 		g.It("Should return error for everything else", func() {
 			_, err := FromFilename("filename.ttt")
 			g.Assert(err != nil).IsTrue("failed to return error")
